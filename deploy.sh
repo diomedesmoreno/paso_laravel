77f92245f716d4ea4377ba2ad1e004d96d7a7d23
diomedes05 <diomedesmoreno05@gmail.com>
2021-11-29 09:51:34 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #Run Databese Migrations
-php artisan migrate:refresh
+# php artisan migrate:refresh
 
 #Run Seeds
 php artisan db:seed --class=UserSeeder
 #php artisan db:seed --class=CountriesSeeder
+php artisan db:seed --class=CountrySeeder
